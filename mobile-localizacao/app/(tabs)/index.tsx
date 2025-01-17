c611b53b364a936cf840722387eb7dae527b7b1d
anderson <anderson361163@gmail.com>
2025-01-16 23:20:32 -0300

    feat: Adicionando a distancia entre 2 pontos;

--- mobile-localizacao/app/(tabs)/index.tsx
+++ mobile-localizacao/app/(tabs)/index.tsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import { View, Button, Text } from 'react-native';
 import * as Location from 'expo-location';
+import { getDistance } from 'geolib';
 
-export default function HomeScreen() {
-
+const App = () => {
   const [location, setLocation] = useState<null | Location.LocationObject>(null);
+  const [distance, setDistance] = useState<number | null>(null);
+
+  const referencePoint = { latitude: -23.56411, longitude: -46.6524 }; // Ponto de referência
 
   const getLocation = async () => {
     const { status } = await Location.requestForegroundPermissionsAsync();
@@ -15,22 +18,32 @@ export default function HomeScreen() {
 
     const currentLocation = await Location.getCurrentPositionAsync({});
     setLocation(currentLocation);
-    console.log(currentLocation);
-    console.log(location?.coords);
 
-
+    const distance = getDistance(
+      { latitude: currentLocation.coords.latitude, longitude: currentLocation.coords.longitude },
+      referencePoint
+    );
+    setDistance(distance / 1000); // Converte de metros para quilômetros
   };
 
   return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Button title="Obter Localização" onPress={getLocation} />
-        {location ? (
-          <Text style={{ color: 'white' }}>
-            Latitude: {location.coords.latitude}, Longitude: {location.coords.longitude}
-          </Text>
-        ) : (
-          <Text>Localização não disponível</Text>
-        )}
-      </View>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Button title="Obter Localização" onPress={getLocation} />
+      {location && (
+        <Text style={{color:'white'}}>
+          Latitude: {location.coords.latitude}, Longitude: {location.coords.longitude}
+        </Text>
+      )}
+      {distance !== null ? (
+        <Text style={{color:'white'}}>
+          A distância até o ponto de referência é: {distance.toFixed(2)} km
+        </Text>
+      ):(
+          <Text style={{color:'white'}}>Localização não disponível</Text>
+      )
+      }
+    </View>
   );
-}
+};
+
+export default App;
